7ebdf7f9ee3eda7f70aa9ba87e214e25af1bf294
Richard Leyshon <richard.leyshon@justice.gov.uk>
2024-12-03 06:39:08 +0000

    feat: Day 3 part 2

--- 03-mull-it-over/challenge.py
+++ 03-mull-it-over/challenge.py
@@ -20,14 +20,35 @@ def calc_results(statements:list) -> int:
     return sum((case[0] * case[1] for case in statements))
 
 
+def redact_puzzle(_input:list) -> list:
+    compiled_puz = "".join(_input)
+    dont_pat = re.compile(r"don't\(\).*?do\(\)")
+    hits = dont_pat.findall(compiled_puz)
+    puz_out = compiled_puz
+    for i in hits:
+        puz_out = puz_out.replace(i, "")
+    return puz_out
+
+
 def get_answers():
     example_pth = here("03-mull-it-over/example.txt")
     example = calc_results(munge_data(load_input(example_pth)))
     input_pth = here("03-mull-it-over/input.txt")
-    _input = calc_results(munge_data(load_input(input_pth)))
+    loaded_input = load_input(input_pth)
+    _input = calc_results(munge_data(loaded_input))
     print("Part one")
     print(f"The answer to the example is {example}")
     print(f"The answer to the input is {_input}")
+
+    example2_pth = here("03-mull-it-over/example2.txt")
+    example2 = load_input(example2_pth)
+    redacted_example = redact_puzzle(example2)
+    example2_out = calc_results(munge_data(redacted_example))
+    redacted_input = redact_puzzle(loaded_input)
+    input2_out = calc_results(munge_data(redacted_input))
+    print("Part two")
+    print(f"The answer to the example is {example2_out}")
+    print(f"The answer to the input is {input2_out}")
 
 
 if __name__ == "__main__":
