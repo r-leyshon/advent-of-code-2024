7ef1bb78519eb008bc069a45a9411478d0087646
Richard Leyshon <richard.leyshon@justice.gov.uk>
2024-12-07 18:13:28 +0000

    feat: Complete day 7 part 2

--- 07-bridge-repair/challenge.py
+++ 07-bridge-repair/challenge.py
@@ -22,13 +22,16 @@ def evaluate_expressions(nums:list, ops:itertools.product):
             out += nums[i + 1]
         elif op == "*":
             out *= nums[i + 1]
+        elif op == "|":
+            repl = str(out) + str(nums[i + 1])
+            out = int(repl)
     return out
 
 
-def solve_part_one(_input:dict):
+def solve_both(_input:dict, _operators:str = "+*"):
     true_vals = list()
     for key, vals in _input.items():
-        op_combos = itertools.product("+*", repeat=len(vals) - 1)
+        op_combos = itertools.product(_operators, repeat=len(vals) - 1)
         for operators in op_combos:
             if evaluate_expressions(nums=vals, ops=operators) == key:
                 true_vals.append(key)
@@ -41,7 +44,13 @@ def solve_part_one(_input:dict):
 def solve():
     example_dat = load_input(here("07-bridge-repair/example.txt"))
     print("Part One")
-    print(f"Answer to example is {solve_part_one(munge_data(example_dat))}")
+    print(f"Answer to example is {solve_both(munge_data(example_dat))}")
+    input_dat = load_input(here("07-bridge-repair/input.txt"))
+    print(f"Answer to part one is {solve_both(munge_data(input_dat))}")
+    print("Part Two")
+    print(f"Answer to example is {solve_both(munge_data(example_dat), '+|*')}")
+    print(f"Answer to part two is {solve_both(munge_data(input_dat), '+|*')}")
+
 
 if __name__ == "__main__":
     solve()
